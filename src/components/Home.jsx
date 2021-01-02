4c3ce6d2aea1c7e017a4f51d28dbb940e8353ad1
jeremyengland <jwengland2808@gmail.com>
2021-01-01 16:57:29 -0800

    Added home page and logout feature

---
+++ src/components/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: this.props.currentUser,
+      userId: this.props.userId,
+      albums: null
+    };
+    this.getAlbums = this.getAlbums.bind(this);
+  }
+
+  // sends request to get a user's albums
+  getAlbums() {
+    axios({
+      method: 'get',
+      url: `/api/users/${this.state.userId}/albums`,
+    })
+      .then((res) => {
+        this.setState({ albums: res.data });
+        console.log(res);
+        console.log('successfully grabbed albums');
+      }, (err) => {
+        console.log(err);
+      })
+  }
+
+  componentDidMount() {
+    this.getAlbums();
+  }
+
+  render() {
+    return (
+      this.state.albums &&
+      <div>
+        <h3>Your Projects:</h3>
+        <p>{this.state.albums[0].title}</p>
+      </div>
+    )
+  }
+}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,65 +1,99 @@
 import React from 'react';
 import axios from 'axios';
+import Home from './Home.jsx'
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      email: '',
       password: '',
-      currentUser: ''
+      currentUser: null,
+      userId: null
     };
     this.attemptLogin = this.attemptLogin.bind(this);
-    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
   }
 
+  // sends request to db to verify
   attemptLogin(e) {
     axios({
       method: 'get',
       url: '/api/users/',
       params: {
-        email: this.state.username,
+        email: this.state.email,
         password: this.state.password
       }
     })
-    .then((res) => {
-      this.setState({currentUser: res.data.username});
-      console.log(res);
-      console.log('successfully logged in');
-    }, (err) => {
-      console.log(err);
-    })
+      .then((res) => {
+        this.setState({
+          currentUser: res.data.username,
+          userId: res.data.id
+        });
+        console.log(res);
+        console.log('successfully logged in');
+      }, (err) => {
+        console.log(err);
+      })
     e.preventDefault();
   }
 
-  handleUserChange(e) {
-    this.setState({username: e.target.value});
-    console.log(this.state.username);
+  // logs current user out
+  handleLogout(e) {
+    this.setState({
+      email: '',
+      password: '',
+      currentUser: null,
+      userId: null
+    })
+    console.log('logged out')
   }
 
+  // changes username stored in state
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+    console.log(this.state.email);
+  }
+
+  // changes password stored in state
   handlePasswordChange(e) {
-    this.setState({password: e.target.value});
+    this.setState({ password: e.target.value });
     console.log(this.state.password);
   }
 
+  // renders login; after login, renders home page
   render() {
     return (
       <div>
         <div>
           <h1>Writevibe</h1>
-          <p>Logged in as {this.state.currentUser}</p>
         </div>
-        <div>
-          <h3>Login</h3>
-          <form onSubmit={this.attemptLogin}>
-            <label>Username</label>
-            <input type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
-            <label>Password</label>
-            <input type="password" data-test="password" value={this.state.password} onChange={this.handlePasswordChange} />
-            <input type="submit" value="Log In" data-test="submit" />
-          </form>
-        </div>
+        {!this.state.currentUser &&
+          <div>
+            <h3>Login</h3>
+            <form onSubmit={this.attemptLogin}>
+              <label>Username</label>
+              <input type="text" data-test="username" value={this.state.email} onChange={this.handleEmailChange} />
+              <label>Password</label>
+              <input type="password" data-test="password" value={this.state.password} onChange={this.handlePasswordChange} />
+              <input type="submit" value="Log In" data-test="submit" />
+            </form>
+          </div>
+        }
+        {this.state.currentUser &&
+          <div>
+            <div>
+              <form onSubmit={this.handleLogout}>
+                <label>Welcome back @{this.state.currentUser}</label>
+                <input type="submit" value="Log Out" data-test="submit" />
+              </form>
+            </div>
+            <div>
+              <Home userId={this.state.userId} currentUser={this.state.currentUser}></Home>
+            </div>
+          </div>
+        }
       </div>
     )
   }
